0d2261713fb2b0f97e3a87ee46e52c1ba8587303
jun <fitjun@foxmail.com>
2025-07-13 09:53:49 +0800

    项目初始结构创建

---
+++ gmall-member/src/test/java/com/jun/gmall/gmallmember/GmallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.gmall.gmallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
